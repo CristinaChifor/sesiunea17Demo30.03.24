df481349e5e68452b1043cc29464662bb9671a10
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-30 22:16:35 +0200

    Work in progress

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const categoryCssClasses = {
     "jewelery": "text-bg-warning",
     "men's clothing": "text-bg-primary",
     "women's clothing": "text-bg-danger"
-}
+} // multiline string
 
 /**
  * product card html:
@@ -34,34 +34,14 @@ const categoryCssClasses = {
 console.log('works');
 
     document.addEventListener('DOMContentLoaded', async () => {
-
-    const listContainer = document.getElementById('products-container');
-    const products = await getProducts();
-
-
-            for (const product of products) {
+        fetch('https://fakestoreapi.com/products')
+            .then((res) => res.json())
+            .then((products) => {
+              for (const product of products) {
                 const productCard = document.createElement('article');
-                productCard.classList.add('col-2-md','m-2','card','cursor-pointer');
-                productCard.innerHTML = `<img src="${product.image}" alt="Product Image">
-                <div class="card-content">
-                    <div class="title">${product.title}</div>
-                    <div class="price">$${product.price}</div>
-                    <span class="badge rounded-pill ${categoryCssClasses[product.category]}">${product.category}</span>
-                    <div class="description">${product.description}</div>
-                </div>`;
-            listContainer.appendChild(productCard); 
-            productCard.addEventListener("click", () => {
-                localStorage.setItem('productID', product.id);
-                window.location.href = './product_page.html';
-            })
-        }
-    });
-       
-    async function getProducts() {
-        const response = await fetch('https://fakestoreapi.com/products');
-        const products = await response.json();
-        return products;
-    };
-
+              }
+                
+            });
+    });  
 
 
