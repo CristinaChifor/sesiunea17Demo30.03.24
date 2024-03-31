408b9ff37124c3919923211b4b9cc25bbc9cfc15
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-31 22:45:31 +0300

    product_page

--- product.js
+++ product.js
@@ -2,26 +2,26 @@ const BASE_URL = 'https://fakestoreapi.com';
 
 document.addEventListener('DOMContentLoaded',
  async () => {
-   const productId = localStorage.getItem('productId');
-   const response = await fetch(`${BASE_URL}/products/${productId}`);
-   const productData = await response.json();
-   console.log(productData);
-   
-   const imageContainer = document.getElementById('img-container');
-   imageContainer.innerHTML = `<img class="img-fluid" src=${productData.image}>`;
-  
-   const title = document.getElementById('product-title');
-   title.textContent = productData.title;
+    const productId = localStorage.getItem('productId');
+    const response = await fetch(`${BASE_URL}/products/${productId}`);
+    const productData = await response.json();
+    console.log(productData);
 
-   const category = document.getElementById('product-categ');
-   category.textContent = productData.category;
+    const imageContainer = document.getElementById('img-container');
+    imageContainer.innerHTML = `<img class="img-fluid" src=${productData.image}>`;
 
-   const price = document.getElementById('product-price');
-   price.textContent = productData.price;
+    const title = document.getElementById('product-title');
+    title.textContent = productData.title;
 
-   const description = document.getElementById('product-description');
-   price.textContent = productData.description;
+    const category = document.getElementById('product-categ');
+    category.textContent = productData.category;
 
-   const reviewCount = document.getElementById('review-count');
-   reviewCount.textContent = `of ${productData.rating.count} reviews`;
-});
+    const price = document.getElementById('product-price');
+    price.textContent = productData.price;
+
+    const description = document.getElementById('product-description');
+    description.textContent = productData.description;
+
+    const reviewCount = document.getElementById('review-count');
+    reviewCount.textContent = `of ${productData.rating.count} reviews`;
+ });
